a0e09846c790fc6e716eb3b2b6b52469cbe99db2
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2016-05-19 15:03:14 +0200

    first commit

--- RPiCam.sh
+++ RPiCam.sh
@@ -30,13 +30,14 @@ case "$1" in
     sudo killall raspimjpeg
     git pull origin master
     sudo apt-get install -y apache2 php5 libapache2-mod-php5 gpac motion zip
-    sudo mkdir -p /var/www/media
+    
+	sudo mkdir -p /var/www/media
     sudo cp -r www/* /var/www/
+	sudo chown -R www-data:www-data /var/www
     if [ -e /var/www/index.html ]; then
       sudo rm /var/www/index.html
     fi
-    sudo chown -R www-data:www-data /var/www
-    
+        
     if [ ! -e /var/www/FIFO ]; then
       sudo mknod /var/www/FIFO p
     fi
@@ -46,21 +47,22 @@ case "$1" in
       sudo mknod /var/www/FIFO1 p
     fi
     sudo chmod 666 /var/www/FIFO1
+	
     sudo chmod 755 /var/www/raspizip.sh
 
     if [ ! -e /var/www/cam.jpg ]; then
       sudo ln -sf /run/shm/mjpeg/cam.jpg /var/www/cam.jpg
     fi
 
-    cat etc/apache2/sites-available/default.1 > etc/apache2/sites-available/default
  
-    sudo cp -r etc/apache2/sites-available/default /etc/apache2/sites-available/
+    sudo cp -r etc/apache2/sites-available/default.1 /etc/apache2/sites-available/default
     sudo chmod 644 /etc/apache2/sites-available/default
+	sudo mkdir -p /etc/apache2/conf.d
     sudo cp etc/apache2/conf.d/other-vhosts-access-log /etc/apache2/conf.d/other-vhosts-access-log
     sudo chmod 644 /etc/apache2/conf.d/other-vhosts-access-log
 
-    sudo cp etc/sudoers.d/RPI_Cam_Web_Interface /etc/sudoers.d/
-    sudo chmod 440 /etc/sudoers.d/RPI_Cam_Web_Interface
+    sudo cp etc/sudoers.d/RPiCam /etc/sudoers.d/
+    sudo chmod 440 /etc/sudoers.d/RPiCam
 
     sudo cp -r bin/raspimjpeg /opt/vc/bin/
     sudo chmod 755 /opt/vc/bin/raspimjpeg
@@ -68,22 +70,18 @@ case "$1" in
       sudo ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
     fi
 
-    cat etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
- 
     sudo cp -r /etc/raspimjpeg /etc/raspimjpeg.bak
-    sudo cp -r etc/raspimjpeg/raspimjpeg /etc/
+    sudo cp -r etc/raspimjpeg/raspimjpeg.1 /etc/raspimjpeg
     sudo chmod 644 /etc/raspimjpeg
     if [ ! -e /var/www/raspimjpeg ]; then
       sudo ln -s /etc/raspimjpeg /var/www/raspimjpeg
     fi
 
-    cat etc/rc_local_run/rc.local.1 > etc/rc_local_run/rc.local
     sudo cp -r /etc/rc.local /etc/rc.local.bak
-    sudo cp -r etc/rc_local_run/rc.local /etc/
+    sudo cp -r etc/rc_local_run/rc.local.1 /etc/rc.local
     sudo chmod 755 /etc/rc.local
 
-    cat etc/motion/motion.conf.1 > etc/motion/motion.conf
-    sudo cp -r etc/motion/motion.conf /etc/motion/
+    sudo cp -r etc/motion/motion.conf.1 /etc/motion/motion.conf
     sudo chmod 640 /etc/motion/motion.conf
     sudo chgrp www-data /etc/motion/motion.conf
     sudo chmod +rrr /etc/motion/motion.conf    
