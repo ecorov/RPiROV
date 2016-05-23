5daabfcfa9156448e5253ad3b9cbdd584d330a27
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2016-05-23 11:36:00 +0200

    first commit

--- www/index.php
+++ www/index.php
@@ -153,18 +153,18 @@
       <script src="js/script.js"></script>
       <script src="js/pipan.js"></script>
    </head>
-   <body onload="setTimeout('init(<?php echo "$mjpegmode, $video_fps, $divider" ?>);', 100);">
+   <body onload="setTimeout('init(0, 25, 1);', 100);">
 
       <div class="container-fluid text-center liveimage">
          <div><img id="mjpeg_dest" <?php if(file_exists("pipan_on")) echo "ontouchstart=\"pipan_start()\""; ?> onclick="toggle_fullscreen(this);" src="/loading.jpg"></div>
-         <div id="main-buttons" <?php echo $displayStyle; ?> >
+         <div id="main-buttons" style="display:block;" >
             <input id="video_button" type="button" class="btn btn-primary">
             <input id="image_button" type="button" class="btn btn-primary">
             <input id="timelapse_button" type="button" class="btn btn-primary">
             <input id="halt_button" type="button" class="btn btn-danger">
          </div>
       </div>
-      <div id="secondary-buttons" class="container-fluid text-center" <?php echo $displayStyle; ?> >
+      <div id="secondary-buttons" class="container-fluid text-center" style="display:block;" >
          <?php 
 		 //if (file_exists("pipan_on")) pipan_controls(); 
 		 ?>
@@ -172,7 +172,7 @@
       </div>
     
       <div class="container-fluid text-center">
-         <div class="panel-group" id="accordion" <?php echo $displayStyle; ?> >
+         <div class="panel-group" id="accordion" style="display:block;" >
 			<input id="shutdown_button" type="button" value="shutdown system" onclick="sys_shutdown();" class="btn btn-danger">
             <input id="reboot_button" type="button" value="reboot system" onclick="sys_reboot();" class="btn btn-danger">
          </div>
