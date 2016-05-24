72bd39df982d3bd286c4f53584688fa430632b15
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2016-05-24 09:58:32 +0200

    first commit

--- www/index.php
+++ www/index.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
 <html>
    <head>
+      <meta name="viewport" content="width=550, initial-scale=1">
       <title>RPi ROV</title>
       <script src="js/script.js"></script>
       <script src="js/pipan.js"></script>
    </head>
    <body onload="setTimeout('init(0, 25, 1);', 100);">
-		 <img id="mjpeg_dest" src="/loading.jpg">
+
+      <div class="container-fluid text-center liveimage">
+   
+		 <div><img id="mjpeg_dest" src="/loading.jpg"></div>
+
          <div id="main-buttons" style="display:block;" >
             <input id="video_button" type="button" class="btn btn-primary">
             <input id="image_button" type="button" class="btn btn-primary">
             <input id="timelapse_button" type="button" class="btn btn-primary">
             <input id="halt_button" type="button" class="btn btn-danger">
-			<a href="preview.php" class="btn btn-default">Download Videos and Images</a>
-         </div
+         </div>
+      </div>
+	  
+      <div id="secondary-buttons" class="container-fluid text-center" style="display:block;" >
+         <a href="preview.php" class="btn btn-default">Download Videos and Images</a>
+      </div>
+    
+      <div class="container-fluid text-center">
          <div class="panel-group" id="accordion" style="display:block;" >
 			<input id="shutdown_button" type="button" value="shutdown system" onclick="sys_shutdown();" class="btn btn-danger">
 			<input id="reboot_button" type="button" value="reboot system" onclick="sys_reboot();" class="btn btn-danger">
          </div>
+      </div>
    </body>
 </html>
