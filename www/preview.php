28ae63e8cd4702b9cd23490c3eb0d002bbb24b1c
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2016-05-25 15:20:14 +0200

    first commit

--- www/preview.php
+++ www/preview.php
@@ -231,7 +231,7 @@
       echo "&nbsp;&nbsp;$fNumber&nbsp;";
       echo "<img src='$fIcon' style='width:24px'/>";
       //echo "<input type='checkbox' name='check_list[]' $sel value='$f' style='float:right;'/>";
-      echo "</legend>";
+      echo "</legend><br>";
       if ($fsz > 0) echo "$fsz Kb $lapseCount $duration"; else echo 'Busy';
       //echo "<br>$fDate<br>$fTime<br>";
       if ($fsz > 0) echo "<a title='$rFile' href='preview.php?preview=$f'>";
