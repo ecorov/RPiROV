fc51d13ecf74dc14214463abe7685ed6089d2c8f
Huidong Tian <tianhd@Huidongs-iMac.local>
2016-05-28 01:07:16 +0200

    test

--- simpleInstall.sh
+++ simpleInstall.sh
@@ -5,11 +5,20 @@ sudo mkdir -p /var/www/media
 sudo chown -R www-data:www-data /var/www
 
 
-sudo mknod /var/www/FIFO p
+if [ ! -e /var/www/FIFO ]; then
+  sudo mknod /var/www/FIFO p
+fi
 sudo chmod 666 /var/www/FIFO
-sudo mknod /var/www/FIFO1 p
+
+if [ ! -e /var/www/FIFO1 ]; then
+  sudo mknod /var/www/FIFO1 p
+fi
 sudo chmod 666 /var/www/FIFO1
-sudo ln -sf /run/shm/mjpeg/cam.jpg /var/www/cam.jpg
+sudo chmod 755 /var/www/raspizip.sh
+
+if [ ! -e /var/www/cam.jpg ]; then
+  sudo ln -sf /run/shm/mjpeg/cam.jpg /var/www/cam.jpg
+fi
 
 # modify "sudo nano /etc/apache2/sites-enabled/000-default.conf ":  "DocumentRoot /var/www "
 sudo cp -r etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf
@@ -21,17 +30,24 @@ sudo chmod 440 /etc/sudoers.d/RPiROV
 
 sudo cp -r bin/raspimjpeg /opt/vc/bin/
 sudo chmod 755 /opt/vc/bin/raspimjpeg
-sudo ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
+if [ ! -e /usr/bin/raspimjpeg ]; then
+  sudo ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
+fi
 
 
 cat etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
 sudo cp -r etc/raspimjpeg/raspimjpeg /etc/
 sudo chmod 644 /etc/raspimjpeg
-sudo ln -s /etc/raspimjpeg /var/www/raspimjpeg
+if [ ! -e /var/www/raspimjpeg ]; then
+  sudo ln -s /etc/raspimjpeg /var/www/raspimjpeg
+fi
 
 sudo chmod 755 /var/www/html/doStuff.py
-sudo cp /usr/bin/python2.7 /usr/bin/pythonRoot
-sudo chmod u+s /usr/bin/pythonRoot
+if [ ! -e /usr/bin/pythonRoot ]; then
+  sudo cp /usr/bin/python2.7 /usr/bin/pythonRoot
+  sudo chmod u+s /usr/bin/pythonRoot
+fi
+
 sudo cp etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf
 
 
@@ -41,16 +57,23 @@ sudo cp -r etc/rc_local_run/rc.local /etc/
 sudo chmod 755 /etc/rc.local
 
 sudo usermod -a -G video www-data
+if [ -e /var/www/uconfig ]; then
+  sudo chown www-data:www-data /var/www/uconfig
+fi
 
 ## edit /etc/passwd to make www-data available
 sudo chsh -s /bin/bash www-data
 
 
+
+sudo killall raspimjpeg
+sudo killall php
+sudo killall motion
 sudo mkdir -p /dev/shm/mjpeg
 sudo chown www-data:www-data /dev/shm/mjpeg
 sudo chmod 777 /dev/shm/mjpeg
 sleep 1;sudo su -c 'raspimjpeg > /dev/null &' www-data
 sleep 1;sudo su -c 'php /var/www/schedule.php > /dev/null &' www-data
-sudo /etc/init.d/lighttpd start
+sudo /etc/init.d/lighttpd restart
 echo "Started"
 
