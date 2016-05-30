77bf7e082dee0721d36aba3d9cee35c4fc445953
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2016-05-30 17:08:33 +0200

    first commit

--- etc/lighttpd/doStuff - Copy.py
+++ etc/lighttpd/doStuff - Copy.py
@@ -1,24 +1,100 @@
-#!/usr/bin/pythonRoot
+#Step 0: Preamble
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+#Code Written by: Salty Scott
+#Current Project: www.rowboboat.com
+#This code is a very basic example of using python to control a spark fun
+# easy driver.  The spark fun easy driver that I am using in this example
+# is connected to a 42HS4013A4 stepper motor and my raspberry pi.  Pin 23
+# is the direction control and pin 24 is the step control.  I am using
+# these components in the www.rowboboat.com project version 2.0 and I
+# hope someone finds this a useful and simple example.
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
 
-import RPi.GPIO as G     
-from flup.server.fcgi import WSGIServer 
-import sys, urlparse
+#Step 1: Import necessary libraries 
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+import sys
+import RPi.GPIO as gpio #https://pypi.python.org/pypi/RPi.GPIO more info
+import time
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+
+#Step 2: Read arguements https://www.youtube.com/watch?v=kQFKtI6gn9Y
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+#read the direction and number of steps; if steps are 0 exit 
+try: 
+    direction = sys.argv[1]
+    steps = int(float(sys.argv[2]))
+except:
+    steps = 0
+
+#print which direction and how many steps 
+print("You told me to turn %s %s steps.") % (direction, steps)
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
 
 
-G.setmode(G.BCM)
-G.setup(17, G.OUT)
+#Step 3: Setup the raspberry pi's GPIOs
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+#use the broadcom layout for the gpio
+gpio.setmode(gpio.BCM)
+#GPIO23 = Direction
+#GPIO24 = Step
+gpio.setup(23, gpio.OUT)
+gpio.setup(24, gpio.OUT)
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
 
 
-def app(environ, start_response):
-	start_response("200 OK", [("Content-Type", "text/html")])
-	i = urlparse.parse_qs(environ["QUERY_STRING"])
-	yield ('&nbsp;') 
-	if "q" in i:
-		if i["q"][0] == "w": 
-			G.output(17, True)  
-		elif i["q"][0] == "s":
-			G.output(17, False)  
+#Step 4: Set direction of rotation
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+#set the direction equal to the appropriate gpio pin
+if direction == 'left':
+    gpio.output(23, True)
+elif direction == 'right':
+    gpio.output(23, False)
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
 
 
-WSGIServer(app).run()
+#Step 5: Setup step counter and speed control variables
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+#track the numebr of steps taken
+StepCounter = 0
 
+#waittime controls speed
+WaitTime = 0.000001
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+
+
+#Step 6: Let the magic happen
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+# Start main loop
+while StepCounter < steps:
+
+    #turning the gpio on and off tells the easy driver to take one step
+    gpio.output(24, True)
+    gpio.output(24, False)
+    StepCounter += 1
+
+    #Wait before taking the next step...this controls rotation speed
+    time.sleep(WaitTime)
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+
+
+#Step 7: Clear the GPIOs so that some other program might enjoy them
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
+#relase the GPIO
+gpio.cleanup()
+#------------------------------------------------------------------------
+#------------------------------------------------------------------------
--- etc/lighttpd/doStuff.py
+++ etc/lighttpd/doStuff.py
@@ -27,3 +27,62 @@ def app(environ, start_response):
 
 WSGIServer(app).run()
 
+
+
+import sys, time, urlparse, RPIO
+from flup.server.fcgi import WSGIServer
+
+S = RPIO.PWM.Servo()
+
+BM_lft = 27
+BM_rgt = 17
+
+SM_stp = 21
+SM_slp = 21
+SM_dir = 21
+
+switch = 20
+
+
+RPIO.setup(SM_stp, RPIO.OUT) # step motor: step
+RPIO.setup(SM_slp, RPIO.OUT) # step motor: direction
+RPIO.setup(SM_slp, RPIO.OUT) # step motor: sleep
+
+
+time.sleep(1)
+S.set_servo(BM_lft, 1000)
+time.sleep(1)
+S.set_servo(BM_rgt, 1000)
+time.sleep(1)
+
+def stepMotor(steps):
+	RPIO.output(SM_slp, False)
+	if steps < 0:
+		RPIO.output(SM_dir, False)
+	elif steps >=0:
+		RPIO.output(SM_dir, True)
+	
+	for (i in step * 100):
+		gpio.output(24, True)
+		gpio.output(24, False)
+		StepCounter += 1
+		time.sleep(WaitTime)
+	RPIO.output(SM_slp, True)	
+	
+def blMotor():
+
+	
+	
+while StepCounter < steps:
+
+    #turning the gpio on and off tells the easy driver to take one step
+    gpio.output(24, True)
+    gpio.output(24, False)
+    StepCounter += 1
+
+    #Wait before taking the next step...this controls rotation speed
+    
+	
+	
+
+
