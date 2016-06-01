1e9eb9c22471f04c04d2982e6f564ccceb99dc25
Huidong Tian <tienhuitung@gmail.com>
2016-06-02 01:34:26 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -13,30 +13,28 @@
 </head>
 
 <body onload="setTimeout('init(0, 25, 1);', 100);">
-  <img id="mjpeg_dest" src="img/loading.jpg">
-  
-  <div id="camera">
+	<img id="mjpeg_dest" src="img/loading.jpg">
+	<div id="camera">
 		<a id="video_button"><img class="icon" src="img/video-1.png"></a>
 		<a id="image_button"><img class="icon" src="img/camera-1.png"></a>
 		<a id="timelapse_button"><img class="icon" src="img/timelapse-1.png"></a>
-  </div>
+		<a id="led_button"><img class="icon" src="img/led-1.png"></a>
+	</div>
   
-  <a id="download" href="preview.php"><img class="icon" src="img/download.png"></a>
+	<a id="download" href="preview.php"><img class="icon" src="img/download.png"></a>
   
-  <div id="system">
-    <a id="showsys"><img class="icon" src="img/power.png"></a><br>
-	  <div id="syscmd" style="display:none;">
-		  <input id="reboot_button" type="button" value="reboot" onclick="sys_reboot();" ><br><br><br>
-		  <input id="shutdown_button" type="button" value="shutdown" onclick="sys_shutdown();" >
-	  </div>
-  </div>
+	<div id="system">
+		<a id="showsys"><img class="icon" src="img/power.png"></a><br>
+		<div id="syscmd" style="display:none;">
+			<input id="reboot_button" type="button" value="reboot" onclick="sys_reboot();" ><br><br><br>
+			<input id="shutdown_button" type="button" value="shutdown" onclick="sys_shutdown();" >
+		</div>
+	</div>
   
-  <div id="boundary"><div id="ctrlrod"></div></div>
-  <p id="debug"></p>
-  <div id="slider"></div>
-	<a id="led_button"><img class="icon" src="img/led-1.png"></a>
-	
-	
+	<div id="boundary"><div id="ctrlrod"></div></div>
+	<p id="debug"></p>
+	<div id="slider"></div>
+
 	<script src="js/ctrl.js"></script>
 </body>
 
