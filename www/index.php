5bce452c56ebe87b932134a33fb3fc698c5e7060
Huidong Tian <tienhuitung@gmail.com>
2016-09-29 11:41:46 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,71 +1,63 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>RPi ROV</title>
-  <link rel="stylesheet" href="css/jquery-ui.css"> 
-  <link rel="stylesheet" href="css/style.css"> 	
-  <script src="js/jquery-1.10.2.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/jquery.ui.touch-punch.min.js"></script>
-  <script src="js/script.js"></script>
-  <script src="js/pipan.js"></script> 
-  <script src="js/md5.js"></script>  
-</head>
-
-<body onload="setTimeout('init(0, 25, 1);', 100);">
-	<div id="CtrlPwd"  style="display:block;">
-		Type password to control this ROV: <br>
-		<input type="text" id="ctrlpwd">
-		<input type="submit" onclick="pwd()" value="Submit">
-	</div>
-	
-	<img id="mjpeg_dest" src="img/loading.jpg"  style="display:none;">
-	
-	<div id="CtrlPannel"  style="display:none;">
-		<div id="camera">
-			<a id="video_button"><img class="icon" src="img/video-1.png"></a>
-			<a id="image_button"><img class="icon" src="img/camera-1.png"></a>
-			<a id="timelapse_button"><img class="icon" src="img/timelapse-1.png"></a>
-			<a id="led_button"><img class="icon" src="img/led-1.png"></a>
-		</div>
-		
-		<a id="download" href="preview.php"><img class="icon" src="img/download.png"></a>
-<!--		
-		<div id="system">
-			<a id="showsys"><img class="icon" src="img/power.png"></a><br>
-			<div id="syscmd" style="display:none;">
-				<input id="reboot_button" type="button" value="reboot" onclick="sys_reboot();" ><br><br><br>
-				<input id="shutdown_button" type="button" value="shutdown" onclick="sys_shutdown();" >
-			</div>
-		</div>
--->		
-
-
-		<select id="drivemode">
-  			<option value="free">Free driving</option>
-  			<option value="yaw">Fix yawing</option>
-		</select>
-		<select id="powerscale">
-  			<option value="slow">Slow speed</option>
-  			<option value="fast">Fast speed</option>
-		</select>
-		
-		<div id="leak"></div>
-		<div id="rovHeading"></div>
-		<div id="boundary"><div id="ctrlrod"></div></div>
-		<div id="depth_slider"></div>
-		<div id="updown"></div>
-		<button id="stopStepMotor" onclick="stopStepMotor()">Stop! </button>
-		<p id="temperature"></p>		
-		<p id="depth_current"></p>
-		<p id="depth_target"></p>
-		<p id="heading_current"></p>
-		<p id="heading_target"></p>
-		<p id="pwm_target"></p>		
-	
-	</div>
-
-	<script src="js/ctrl.js"></script>
-</body>
-
+    <head>
+        <title>RPi ROV</title>
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/jquery-1.10.2.js"></script>
+        <script src="js/jquery-ui.js"></script>
+        <script src="js/jquery.ui.touch-punch.min.js"></script>
+        <script src="js/script.js"></script>
+        <script src="js/pipan.js"></script> 
+        <script src="js/md5.js"></script>  
+    </head>
+    <body onload="setTimeout('init(0, 25, 1);', 100);">
+        <div id="CtrlPwd"  style="display:block;">
+            Type password to control this ROV: <br>
+            <input type="text" id="ctrlpwd">
+            <input type="submit" onclick="pwd()" value="Submit">
+        </div>
+        <img id="mjpeg_dest" src="img/loading.jpg"  style="display:none;">
+        <div id="CtrlPannel"  style="display:none;">
+            <div id="camera">
+                <a id="video_button"><img class="icon" src="img/video-1.png"></a>
+                <a id="image_button"><img class="icon" src="img/camera-1.png"></a>
+                <a id="timelapse_button"><img class="icon" src="img/timelapse-1.png"></a>
+                <a id="led_button"><img class="icon" src="img/led-1.png"></a>
+            </div>
+            <a id="download" href="preview.php"><img class="icon" src="img/download.png"></a>
+            <!--		
+                <div id="system">
+                	<a id="showsys"><img class="icon" src="img/power.png"></a><br>
+                	<div id="syscmd" style="display:none;">
+                		<input id="reboot_button" type="button" value="reboot" onclick="sys_reboot();" ><br><br><br>
+                		<input id="shutdown_button" type="button" value="shutdown" onclick="sys_shutdown();" >
+                	</div>
+                </div>
+                -->		
+            <select id="drivemode">
+                <option value="free">Free driving</option>
+                <option value="yaw">Fix yawing</option>
+            </select>
+            <select id="powerscale">
+                <option value="slow">Slow speed</option>
+                <option value="fast">Fast speed</option>
+            </select>
+            <div id="leak"></div>
+            <div id="rovHeading"></div>
+            <div id="boundary">
+                <div id="ctrlrod"></div>
+            </div>
+            <div id="depth_slider"></div>
+            <div id="updown"></div>
+            <button id="stopStepMotor" onclick="stopStepMotor()">Stop! </button>
+            <p id="temperature"></p>
+            <p id="depth_current"></p>
+            <p id="depth_target"></p>
+            <p id="heading_current"></p>
+            <p id="heading_target"></p>
+            <p id="pwm_target"></p>
+        </div>
+        <script src="js/ctrl.js"></script>
+    </body>
 </html>
